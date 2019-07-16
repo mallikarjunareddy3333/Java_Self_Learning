865bceb422348702370c95364d204f9e0d265f39
giddamr <giddamr@GIDDAMR2C5S542.opentext.net>
2019-07-16 09:44:45 +0530

    - formatted the code

--- Programs/arjun/prgms/regexp/ValidIdentifiers.java
+++ Programs/arjun/prgms/regexp/ValidIdentifiers.java
@@ -6,13 +6,20 @@ import java.util.regex.Pattern;
 public class ValidIdentifiers {
 
 	public static void main(String[] args) {
+		
 		String pattern = "[a-zA-Z][a-zA-Z0-9-#]+";
+		
 		Pattern p = Pattern.compile(pattern);
 		Matcher m = p.matcher("?arjun");
+		
 		if (m.find() && m.group().equals("?arjun")) {
+			
 			System.out.println("valid identifier");
+			
 		} else {
+			
 			System.out.println("Invalid identifier");
+			
 		}
 	}
 
